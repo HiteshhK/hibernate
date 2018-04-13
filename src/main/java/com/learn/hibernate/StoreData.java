734e1468776b660abc3bc87ee5dc15503168e155
Detesh.K <kaushikhitesh002@gmail.com>
2018-04-13 12:58:10 +0530

    Complete Crud Operation using Hibernate

--- src/main/java/com/learn/hibernate/StoreData.java
+++ src/main/java/com/learn/hibernate/StoreData.java
@@ -10,90 +10,74 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;  
   
 public class StoreData {  
+	private static boolean isEnd = true;
+	private static SessionFactory factory;
+	private static Scanner sc;
 	public static void main(String[] args) {  
-
-		Scanner sc = new Scanner(System.in);
-		//creating configuration object  
-		Configuration cfg=new Configuration();  
-		cfg.configure("/com/learn/hibernate/hibernate.cfg.xml");//populates the data of the configuration file  
-
+		sc = new Scanner(System.in);
+		
 		//creating seession factory object  
-		SessionFactory factory=cfg.buildSessionFactory();  
+		factory = SessionConnect.connectSession();  
 
 		//creating session object  
+		System.out.println("*-------------------------------------*");
+		while(isEnd) {
+			System.out.println("/n 1.Press 1 to add new user");
+			System.out.println("/n 2.Press 2 to View all users");
+			System.out.println("/n 3.Press 3 to update a user");
+			System.out.println("/n 4.Press 4 to delete a user");
+			System.out.println("/n 5.Press 5 to exit");
+			System.out.println("Enter Your choice");
+			int choice = sc.nextInt();
+			switch(choice) {
+				case 1: addUser();break;
+				case 2: readDataFromTable();break;
+				case 3: updateUser(); break;
+				case 4: deleteUser();break;
+				case 5: isEnd = false;break;
+				default:System.out.println("Invalid choice");break;
+			}
+			
+		} 
+		if(isEnd == false) {
+			System.exit(0);
+		}
+		System.out.println("successfully saved");  
+	}  
+	
+	private static void addUser() {
+		
 		Session session=factory.openSession();  
 
 		//creating transaction object  
 		Transaction t=session.beginTransaction();  
 
-//		System.out.println("Enter Number of Students");
-//		int num = sc.nextInt();
-//		
-//		Student e1;  
-//		for(int i = 1; i <= num ; i++)
-//		{
-//			e1 = new Student();
-//			System.out.println("Enter roll number");
-//			int rno = sc.nextInt();
-//			e1.setRollNo(rno);
-//			System.out.println("Enter Name:");
-//			String name = sc.next();
-//			e1.setName(name);  
-//			sc.nextLine();
-//			System.out.println("Enter Age:");
-//			int age = sc.nextInt();
-//			e1.setAge(age);
-//			session.save(e1);//persisting the object 
-//			
-//		}
-		  
-
+		System.out.println("Enter Number of Students");
+		int num = sc.nextInt();
 		
-		readDataFromTable();
-		
-//		System.out.println("Delete a record?");
-		System.out.println("Update a record?");
-		String ans = sc.next();
-		sc.nextLine();
-		if(ans.equalsIgnoreCase("yes")) {
-			System.out.println("Enter a record number");
+		Student e1;  
+		for(int i = 1; i <= num ; i++)
+		{
+			e1 = new Student();
+			System.out.println("Enter roll number");
 			int rno = sc.nextInt();
-			Student s = session.load(Student.class, rno);
-			System.out.println("update age of: "+s.getAge()+" to : ");
-			int age = sc.nextInt();
-			s.setAge(age);
-			System.out.println("Update name : "+s.getName()+" to: ");
+			e1.setRollNo(rno);
+			System.out.println("Enter Name:");
 			String name = sc.next();
+			e1.setName(name);  
 			sc.nextLine();
-			
-			s.setName(name);
-//			session.save(s);
-			
-			t.commit();
-			readDataFromTable();
+			System.out.println("Enter Age:");
+			int age = sc.nextInt();
+			e1.setAge(age);
+			session.save(e1);//persisting the object 
 		}
-		else if(ans.equalsIgnoreCase("no")) {
-			System.out.println("no selected");
-		}
-		else {
-			System.out.println("Wrong choice");
-		}
-		//t.commit();//transaction is committed  
-		session.close();  
-
-		System.out.println("successfully saved");  
-
-	}  
-	
-	
-	
+		t.commit();
+		session.close();
+	}
 	
 	private static void readDataFromTable() {
-		Configuration cfg=new Configuration();  
-		cfg.configure("/com/learn/hibernate/hibernate.cfg.xml");//populates the data of the configuration file  
 
 		//creating seession factory object  
-		SessionFactory factory=cfg.buildSessionFactory();  
 
 		//creating session object  
 		Session session=factory.openSession();  
@@ -107,5 +91,39 @@ public class StoreData {
 			System.out.println("------------------------");
 			System.out.println("| "+ (i+1) + " | "+e1.getName() + " | " + e1.getAge()+" | "+e1.getRollNo());
 		}
+	}
+	private static void updateUser() {
+		Session session=factory.openSession();  
+		//creating transaction object  
+		Transaction t=session.beginTransaction();
+		System.out.println("Enter roll no");
+		int rno = sc.nextInt();
+		Student s = session.load(Student.class, rno);
+		System.out.println("update age of: "+s.getAge()+" to : ");
+		int age = sc.nextInt();
+		s.setAge(age);
+		System.out.println("Update name : "+s.getName()+" to: ");
+		String name = sc.next();
+		sc.nextLine();
+
+		s.setName(name);
+		//			session.save(s);ye
+
+		t.commit();
+		readDataFromTable();
+		session.close();
+		//t.commit();//transaction is committed  
+	}
+	private static void deleteUser() {
+		Session session=factory.openSession();  
+		//creating transaction object  
+		Transaction t=session.beginTransaction();
+		System.out.println("Enter a roll number");
+		int rno = sc.nextInt();
+		Student s = session.load(Student.class, rno);
+		session.delete(s);
+		t.commit();
+		readDataFromTable();
+		session.close();
 	}
 }  
